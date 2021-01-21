8afc4af2704d6714385fb791e4314b35873b283e
eliotks <Ntnu1580>
2021-01-21 01:21:16 +0100

    satt opp et skjellett til et fullverdig spill av solitaire

--- SimWorld/peg_board.py
+++ SimWorld/peg_board.py
@@ -1,6 +1,6 @@
 
 
-# initializes a triangle shaped grid
+# Static initializer of a triangle shaped grid
 def create_triangle_grid(size, empty_nodes):
     grid = []
     for i in range(size):
@@ -14,7 +14,7 @@ def create_triangle_grid(size, empty_nodes):
     return grid
 
 
-# initializes a diamond shaped grid
+# Static initializer of a diamond shaped grid
 def create_diamond_grid(size, empty_nodes):
     grid = []
     for i in range(size):
@@ -28,21 +28,57 @@ def create_diamond_grid(size, empty_nodes):
     return grid
 
 
+# Returns a list of all target node coordinates, assumed to be in the middle of the board
+def find_target_nodes(size, is_diamond):
+    if is_diamond:
+        if size % 2 == 0:
+            return [
+                [size / 2 - 1, size / 2 - 1],
+                [size / 2 - 1, size / 2],
+                [size / 2, size / 2 - 1],
+                [size / 2, size / 2],
+            ]
+        else:
+            return [[(size - 1) / 2, (size - 1) / 2]]
+    else:
+        target_nodes_for_every_size = {
+            4: [[2, 1]],
+            5: [[2, 1], [3, 1], [3, 2]],
+            6: [[3, 1], [3, 2], [4, 2]],
+            7: [[4, 2]],
+            8: [[4, 2], [5, 2], [5, 3]]
+        }
+        return target_nodes_for_every_size[size]
+
+
 class PegBoard:
-    def __init__(self, size, is_diamond, empty_nodes):
+
+    def __init__(self, size, is_diamond, empty_nodes, target_nodes=False):
+
+        # The size of the board is decided by the number of nodes at each edge of the board,
+        # and ranges from 4 to 8 for triangles and 3 to 6 for diamonds by the task description
         self.size = size
+
+        # Decides the shape of the board, where the shape is either diamond or triangle
         self.isDiamond = is_diamond
 
-        # board has diamond shape
+        # The grid is a list of lists of nodes representing the board as a coordinate system, where a
+        # node value of 1 represents a peg, while a node value of 0 represents an empty node (hole)
         if is_diamond:
             self.grid = create_diamond_grid(size, empty_nodes)
-
-        # board has triangle shape
+            self.total_pegs_left = (size - 1) * (size - 1) - len(empty_nodes)
         else:
             self.grid = create_triangle_grid(size, empty_nodes)
+            self.total_pegs_left = (size * (size + 1)) / 2 - len(empty_nodes)
 
-    # returns all possible actions given the current board
-    def get_possible_actions(self):
+        # For a game of solitaire to be won, the final peg must end up in on of the target nodes
+        if target_nodes:
+            self.target_nodes = target_nodes
+        else:
+            self.target_nodes = find_target_nodes(size, is_diamond)
+
+    # Returns all possible actions for the current board
+    def all_possible_actions(self):
         possible_actions = []
         grid = self.grid
         size = self.size
@@ -52,67 +88,54 @@ class PegBoard:
                 for j in range(size):
                     if grid[i][j] == 1:
 
-                        # direction: up
+                        # Direction: up
                         if i > 1 and grid[i - 1][j] == 1 and grid[i - 2][j] == 0:
                             possible_actions.append([[i, j], [i - 1, j], [i - 2, j]])
 
-                        # direction: right
+                        # Direction: right
                         if j < size - 2 and grid[i][j + 1] == 1 and grid[i][j + 2] == 0:
                             possible_actions.append([[i, j], [i, j + 1], [i, j + 2]])
 
-                        # direction: down & right
+                        # Direction: down & right
                         if i < size - 2 and j < size - 2 and grid[i + 1][j + 1] == 1 and grid[i + 2][j + 2] == 0:
                             possible_actions.append([[i, j], [i + 1, j + 1], [i + 2, j + 2]])
 
-                        # direction: down
+                        # Direction: down
                         if i < size - 2 and grid[i + 1][j] == 1 and grid[i + 2][j] == 0:
                             possible_actions.append([[i, j], [i + 1, j], [i + 2, j]])
 
-                        # direction: left
+                        # Direction: left
                         if j > 1 and grid[i][j - 1] == 1 and grid[i][j - 2] == 0:
                             possible_actions.append([[i, j], [i, j - 1], [i, j - 2]])
 
-                        # direction: up & left
+                        # Direction: up & left
                         if i > 1 and j > 1 and grid[i - 1][j - 1] == 1 and grid[i - 2][j - 2] == 0:
                             possible_actions.append([[i, j], [i - 1, j - 1], [i - 2, j - 2]])
 
         return possible_actions
 
-    # executes the provided action on the current board
+    # Executes the provided action on the current board
     def execute_action(self, action):
         self.grid[action[0][0]][action[0][1]] = 0
         self.grid[action[1][0]][action[1][1]] = 0
         self.grid[action[2][0]][action[2][1]] = 1
+        self.total_pegs_left -= 1
 
-    # returns the next state of the board after the provided action is taken
-    def next_state(self, action):
-        grid = self.grid.copy()
-        grid[action[0][0]][action[0][1]] = 0
-        grid[action[1][0]][action[1][1]] = 0
-        grid[action[2][0]][action[2][1]] = 1
-        return grid
+    # Needed to determine if a game of solitaire is won
+    def target_nodes_contain_peg(self):
+        for node in self.target_nodes:
+            if self.grid[node[0]][node[1]] == 1:
+                return True
+        return True
+
+    # Returns the next state of the board after the provided action is taken
+    # def next_state(self, action):
+    #     grid = self.grid.copy()
+    #     grid[action[0][0]][action[0][1]] = 0
+    #     grid[action[1][0]][action[1][1]] = 0
+    #     grid[action[2][0]][action[2][1]] = 1
+    #     return grid
 
     def print_board(self):
         for i in range(self.size):
-            print(self.grid[i], i)
-
-
-if __name__ == '__main__':
-    board = PegBoard(4, True, [[3, 3]])
-
-    print(board.print_board())
-
-    print('Actions:')
-    pa = board.get_possible_actions()
-    for i in pa:
-        print(i)
-
-    action = [[1, 1], [2, 2], [3, 3]]
-    print('Excecuting action, ', action)
-    board.execute_action(action)
-
-    print(board.print_board())
-
-
-
-
+            print(self.grid[i])
--- SimWorld/peg_player.py
+++ SimWorld/peg_player.py
@@ -1,3 +1,44 @@
 from SimWorld.peg_board import PegBoard
+import random
 
-# class PegPlayer:
+
+class PegPlayer:
+    def __init__(self, peg_board):
+        self.peg_board = peg_board
+
+    # Decides the next action to be executed
+    def decide_next_action(self):
+        all_possible_actions = self.peg_board.all_possible_actions()
+        if len(all_possible_actions) == 0:
+            return False
+        return all_possible_actions[random.randrange(len(all_possible_actions))]
+
+    # Executes the provided action on the current board
+    def execute_action(self, action):
+        self.peg_board.execute_action(action)
+
+    # Return if the game of solitaire is won or not
+    def has_won(self):
+        return self.peg_board.total_pegs_left == 1 and self.peg_board.target_nodes_contain_peg
+
+
+if __name__ == '__main__':
+    peg_board = PegBoard(4, True, [[3, 3]])
+    peg_player = PegPlayer(peg_board)
+
+    game_over = False
+
+    while not game_over:
+        peg_board.print_board()
+        print("")
+        action = peg_player.decide_next_action()
+        if action:
+            peg_player.execute_action(action)
+        else:
+            game_over = True
+
+    if peg_player.has_won():
+        print("Du har vunnet!")
+    else:
+        print("Du vant dessverre ikke denne gang.")
+
